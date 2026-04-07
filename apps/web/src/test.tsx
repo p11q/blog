96b9f00977bd729c9d813d676e5bf2630536119e
kimanowka <etokimanowka33@rambler.ru>
2026-04-07 22:15:26 +0300

    new react

---
+++ apps/web/src/test.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { Button } from './components/ui/button';
+
+export const Test = () => {
+  const query = useQuery({
+    queryKey: ['articles'],
+    queryFn: async () => {
+      const response = await fetch(`http://127.0.0.1:3000'}/articles`);
+      return await response.json();
+    },
+  });
+
+  console.log(query.data);
+
+  return (
+    <Button size="lg" variant="destructive">
+      BUTTOBN
+    </Button>
+  );
+};
