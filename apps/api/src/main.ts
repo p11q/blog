c677bb9c1de477e1debe6fb3dc7af9fae20ca88e
kimanowka <etokimanowka33@rambler.ru>
2026-04-07 21:08:10 +0300

    init project

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors();
 
   const config = app.get(ConfigService);
 
--- apps/web/apps/web/src/main.tsx
+++ apps/web/apps/web/src/main.tsx
@@ -1,25 +1,36 @@
-import { createRoot } from "react-dom/client";
-import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import { Counter, Header } from '@repo/ui';
+import { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import typescriptLogo from '/typescript.svg';
 
-const App = () => (
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
+const App = () => {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL ?? 'http://127.0.0.1:3000'}/articles`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }, []);
+
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img
+          src={typescriptLogo}
+          className="logo vanilla"
+          alt="TypeScript logo"
+        />
+      </a>
+      <Header title="Web" />
+      <div className="card">
+        <Counter />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-createRoot(document.getElementById("app")!).render(<App />);
+createRoot(document.getElementById('app')!).render(<App />);
