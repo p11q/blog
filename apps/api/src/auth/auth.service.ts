0fbc2dfecbf34bb175d04ccc851c045d46fdfce1
p11q <aonch12345@icloud.com>
2026-06-23 06:17:42 +0300

    pnpm format

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -47,7 +47,7 @@ export class AuthService {
     return new SignInResponceDto(accessToken, refreshTokenCreated.token);
   }
 
-  async refreshToken(token: string): Promise<SignInResponceDto> {
+  async refreshToken(token: string) {
     const now = new Date();
 
     const refreshToken = await this.refreshTokenRepo.findOne({
